e31ad30c638f02dcba0b392c4dbb3b906d5abb2d
dliiva <diana.liiva@ut.ee>
2022-03-19 19:24:17 +0200

    mapstuff

--- script.js
+++ script.js
@@ -49,6 +49,17 @@
     });
     
     // forms
+
+    /*
+lisage interaktiivsus Tarne hind nupule: nupul vajutamisel peaks kasutaja mugaval vormil teada saama, 
+palju tal tuleb tarne eest maksta (valikute hinnad on koodis kommentaaridena kirjas);
+
+täiendage olemasolev vorm raadionuppudega (radiobutton), mille sisu valige ise (aga olgu ikkagi kuidagi seotud tarnega), 
+ja kujundage see style.css failis (css-faili uue osa juurde lisage kommentaar siin on minu looming);
+
+lisage vormile sisendi kontroll: tekstiväljad ei tohi olla tühjad, ei tohi sisaldada numbreid, 
+üks raadionuppudest peab olema valitud (vastasel juhul visatakse ette alert aken) jne.;
+    */
     
     document.getElementById("form").addEventListener("submit", estimateDelivery);
     
@@ -82,7 +93,8 @@
 
 // map
 
-let mapAPIKey = "AqLLRE37SJGqIxXEYxezPUa6fF2oCzl3cvG4n05FtFIVBrotBYxchpMYYpwuxBak";
+//let mapAPIKey = "AqLLRE37SJGqIxXEYxezPUa6fF2oCzl3cvG4n05FtFIVBrotBYxchpMYYpwuxBak";
+let mapAPIKey = "AoJ_VpiJhtMIMmsj-qlta6eKr4_F5rmFmegRwqxBBN5-UdVsLISNm_96NGF29XGC";
 
 let map;
 
@@ -90,28 +102,79 @@ function GetMap() {
     
     "use strict";
 
-    let centerPoint = new Microsoft.Maps.Location(
-            58.38104, 
-            26.71992
+    let centerPointTartu = new Microsoft.Maps.Location(
+        58.38104, 
+        26.71992
         );
+
+    let centerPointLiiva = new Microsoft.Maps.Location(
+        58.53973,
+        23.01958
+        );
+
+    let mapCenterPoint = new Microsoft.Maps.Location(
+        58.23438,
+        24.77417
+    );
 
     map = new Microsoft.Maps.Map("#map", {
         credentials: mapAPIKey,
-        center: centerPoint,
-        zoom: 14,
+        center: mapCenterPoint,
+        zoom: 8,
         mapTypeId: Microsoft.Maps.MapTypeId.road,
         disablePanning: true
     });
+
     
-    let pushpin = new Microsoft.Maps.Pushpin(centerPoint, {
-            title: 'Tartu Ülikool',
-            //subTitle: 'Hea koht',
-            //text: 'UT'
+    
+    let pushpinT = new Microsoft.Maps.Pushpin(centerPointTartu, {
+        enableClickedStyle: true,
+        title: 'Tartu Ülikool',
         });
 
-    map.entities.push(pushpin);
+
+    //Add a click event handler to the pushpin.
+    Microsoft.Maps.Events.addHandler(pushpinT, 'click', pushpinClicked);
+    
+    map.entities.push(pushpinT);
+
+    let pushpinL = new Microsoft.Maps.Pushpin(centerPointLiiva, {
+        enableClickedStyle: true,
+        title: 'Liiva',
+    });
+
+    map.entities.push(pushpinL);
+
+    let infoboxT = new Microsoft.Maps.Infobox(centerPointTartu, {
+        visible: true,
+        title: 'Ülikool',
+
+    });
+
+    let infoboxL = new Microsoft.Maps.Infobox(centerPointLiiva, {
+        visible: false,
+        title: 'Saaremaa',
+
+    });
+
+    map.entities.push(infoboxT);
+    map.entities.push(infoboxL);
+
+    Microsoft.Maps.Events.addHandler(pushpinL, 'click', pushpinClicked(pushpinL));
+    Microsoft.Maps.Events.addHandler(pushpinT, 'click', pushpinClicked(pushpinT));
+
 
 }
+
+function pushpinClicked(e) {
+    e.setOptions({
+        visible: true
+    });
+    
+}
+
+
+
 
 // https://dev.virtualearth.net/REST/v1/Locations?q=1000 Vin Scully Ave, Los Angeles,CA&key=YOUR_KEY_HERE
 
